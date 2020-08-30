491563d3affe74222944a7f195560ef8b60d933d
ShreyasSavanoor <shreyas.savanoor2612@gmail.com>
2020-08-30 23:25:41 +0530

    settimg up components + fonts + scss variables

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+
+import HomePage from './pages/homepage/homepage.component';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      <HomePage></HomePage>
     </div>
   );
 }
