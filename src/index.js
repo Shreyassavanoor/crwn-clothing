44015c685ca813bbe79e6c156d7fe606ecd33a1c
ShreyasSavanoor <shreyas.savanoor2612@gmail.com>
2020-08-31 23:18:42 +0530

    routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+
+import "./index.scss";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
