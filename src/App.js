9af54e9cc26fb447a2b0d9f169bd0b065b48e7db
ShreyasSavanoor <shreyas.savanoor2612@gmail.com>
2020-09-01 22:11:46 +0530

    shop page done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.scss';
 
 function App() {
   return (
     <div>
-      <HomePage></HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import './shop.styles.scss';
+
+import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+const ShopPage = () => {
+    const [collections, setCollections] = useState(SHOP_DATA);
+
+    return (
+        <div className="shop-page">
+            {
+                collections.map(({id, ...otherCollectionProps}) => (
+                    <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
+                ))
+            }
+        </div>
+    );
+}
+
+export default ShopPage;
